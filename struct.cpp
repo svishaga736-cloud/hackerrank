dad7ab7e98198220530ec24038e8ac9d69062c24
svishaga736-cloud <svishaga736@gmail.com>
2026-03-03 19:53:28 +0530

    struct

---
+++ struct.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
